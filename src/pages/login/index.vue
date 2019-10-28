5c07f0cdab9728b9b015e4cd71c4d4771e660f6b
learnshare <learnshare@126.com>
2019-10-28 15:47:27 +0800

    ui: element-ui and custom theme.

---
+++ src/pages/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="page-login">
+    <ui-login
+        type="login"></ui-login>
+  </div>
+</template>
+
+<script>
+import UiLogin from '../../components/ui-login/index.vue';
+
+export default {
+  name: 'PageLogin',
+  components: {
+    'ui-login': UiLogin,
+  },
+};
+</script>
