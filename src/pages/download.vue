ba0c92d6ad8d9de212deb7feb14260c3a47efa4f
learnshare <learnshare@126.com>
2019-11-05 17:45:49 +0800

    feat: page-download.

--- src/pages/download.vue
+++ src/pages/download.vue
@@ -1,16 +1,33 @@
 <template>
   <main-frame>
-    <div class="page-download">Page Download</div>
+    <div class="page-upload page-download">
+      <upload-list
+          :data="data" />
+    </div>
   </main-frame>
 </template>
 
 <script>
+import Mock from '../api/mock';
+
 import MainFrame from '../components/main-frame/index.vue';
+import UploadList from '../components/upload-list/index.vue';
 
 export default {
   name: 'Download',
   components: {
     MainFrame,
+    UploadList,
+  },
+  data() {
+    return {
+      data: Mock.uploads,
+    };
   },
 };
 </script>
+
+<style
+    src="./upload/index.scss"
+    lang="scss"
+    scoped></style>
