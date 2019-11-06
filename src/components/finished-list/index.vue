ab1eb4565f23548a53d3998797df1753f65b6584
learnshare <learnshare@126.com>
2019-11-06 15:41:15 +0800

    feat: page-finished and page trash.

---
+++ src/components/finished-list/index.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="file-list finished-list">
+    <div class="toolbar">
+      <el-button>
+        <i class="iconfont icon-trash"></i>
+        <span>清空全部记录</span>
+      </el-button>
+    </div>
+    <el-table
+        class="file-table finished-table"
+        :data="data"
+        :height="tableHeight">
+      <el-table-column
+          type="selection"
+          width="64"></el-table-column>
+      <el-table-column
+          prop="type"
+          label="全选"
+          width="48">
+        <template>
+          <i class="iconfont icon-folder-add"></i>
+        </template>
+      </el-table-column>
+      <el-table-column
+          label="文件名称">
+        <template
+            slot-scope="scope">
+          <a
+              v-if="scope.row.type === 'folder'"
+              :href="`/?path=${scope.row.id}`">{{ scope.row.name }}</a>
+          <span
+              v-else>{{ scope.row.name }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+          label="文件大小"
+          width="100">
+        <template
+            slot-scope="scope">
+          <span>{{ scope.row.size | filesize }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+          label="状态"
+          width="200">
+        <template
+            slot-scope="scope">
+          <span>下载完成 {{ scope.row.utime | time('yyyy/MM/dd HH:mm') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column
+          width="70">
+        <template>
+          <div>
+            <i class="iconfont icon-trash"></i>
+          </div>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import {
+  Button,
+  Table,
+  TableColumn,
+} from 'element-ui';
+
+export default {
+  name: 'UploadList',
+  components: {
+    'el-button': Button,
+    'el-table': Table,
+    'el-table-column': TableColumn,
+  },
+  props: {
+    // 类型（upload: 全部文件, download: 我的收藏）
+    type: {
+      validator: (value) => {
+        const values = [
+          'upload',
+          'download',
+        ];
+
+        return (values.indexOf(value) >= 0);
+      },
+    },
+    data: Array,
+  },
+  data() {
+    return {
+      tableHeight: null,
+    };
+  },
+  mounted() {
+    window.addEventListener('resize', this.calcTableHeight);
+
+    this.calcTableHeight();
+  },
+  beforeDestroy() {
+    window.removeEventListener('resize', this.calcTableHeight);
+  },
+  methods: {
+    calcTableHeight() {
+      this.tableHeight = window.innerHeight - 124;
+    },
+  },
+};
+</script>
+
+<style
+    src="../file-list/index.scss"
+    lang="scss"
+    scoped></style>
--- src/pages/trash.vue
+++ src/pages/trash.vue
@@ -1,16 +1,33 @@
 <template>
   <main-frame>
-    <div class="page-trash">Page Trash</div>
+    <div class="page-upload page-trash">
+      <trash-list
+          :data="data" />
+    </div>
   </main-frame>
 </template>
 
 <script>
+import Mock from '../api/mock';
+
 import MainFrame from '../components/main-frame/index.vue';
+import TrashList from '../components/trash-list/index.vue';
 
 export default {
   name: 'Trash',
   components: {
     MainFrame,
+    TrashList,
+  },
+  data() {
+    return {
+      data: Mock.files,
+    };
   },
 };
 </script>
+
+<style
+    src="./upload/index.scss"
+    lang="scss"
+    scoped></style>
