eb00ee7495bee819e3481cb7af10e8dbeb7415d3
learnshare <learnshare@126.com>
2019-11-06 15:41:15 +0800

    feat: page-finished and page trash.

--- src/pages/finished.vue
+++ src/pages/finished.vue
@@ -1,16 +1,33 @@
 <template>
   <main-frame>
-    <div class="page-finished">Page Finished</div>
+    <div class="page-upload page-finished">
+      <finished-list
+          :data="data" />
+    </div>
   </main-frame>
 </template>
 
 <script>
+import Mock from '../api/mock';
+
 import MainFrame from '../components/main-frame/index.vue';
+import FinishedList from '../components/finished-list/index.vue';
 
 export default {
   name: 'Finished',
   components: {
     MainFrame,
+    FinishedList,
+  },
+  data() {
+    return {
+      data: Mock.files,
+    };
   },
 };
 </script>
+
+<style
+    src="./upload/index.scss"
+    lang="scss"
+    scoped></style>
