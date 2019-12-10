57373c9aa5ecc3ce035def033c507cb8441b601c
learnshare <learnshare@126.com>
2019-12-10 20:21:18 +0800

    feat: erasure-code and ipfs.

--- src/main.js
+++ src/main.js
@@ -14,6 +14,8 @@ import './style/reset/index.scss';
 // VueContext custom style
 import './style/vue-context.scss';
 
+import './utils/erasure';
+
 Vue.use(filters);
 
 Vue.config.productionTip = false;
