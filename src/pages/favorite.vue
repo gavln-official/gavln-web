4f7d719adcfa53de59a940b5c0f0507787f40bc7
TheC <i@thec.me>
2020-01-04 00:42:32 -0800

    feat: fav list

--- src/pages/favorite.vue
+++ src/pages/favorite.vue
@@ -1,18 +1,19 @@
 <template>
   <main-frame>
-    <div class="page-favorite">
+    <div class="page-favorite" v-loading="loading">
       <file-list
           type="favorite"
+          path="favorite"
+          @reload="getList"
           :data="data"></file-list>
     </div>
   </main-frame>
 </template>
 
 <script>
-import Mock from '../api/mock';
-
 import MainFrame from '../components/main-frame/index.vue';
 import FileList from '../components/file-list/index.vue';
+import FavAPI from '../api/fav';
 
 export default {
   name: 'Favorite',
@@ -22,8 +23,28 @@ export default {
   },
   data() {
     return {
-      data: Mock.files,
+      loading: false,
+      data: null,
     };
+  },
+  methods: {
+    async getList() {
+      if (this.loading) {
+        return false;
+      }
+      this.loading = true;
+      const r = await FavAPI.getList();
+      this.loading = false;
+      if (r.code !== 0) {
+        this.$message.error(r.msg);
+      } else {
+        this.data = r.data;
+      }
+      return true;
+    },
+  },
+  mounted() {
+    this.getList();
   },
 };
 </script>
