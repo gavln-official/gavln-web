096addb00fc474ec89011566de65bfe47b11ee22
TheC <i@thec.me>
2020-01-24 02:00:42 -0800

    bugfix: time filter

--- src/filters/time.js
+++ src/filters/time.js
@@ -2,6 +2,10 @@ import {
   format as dateFormat,
 } from 'date-fns';
 
-const time = (value, format) => dateFormat(new Date(value * 1000), format);
+const time = (_value, format) => {
+  // 后端返回的时间戳不是ms为单位，统一转成js用的13位时间戳
+  const value = _value * (10 ** (13 - String(_value).length));
+  return dateFormat(new Date(value), format);
+};
 
 export default time;
